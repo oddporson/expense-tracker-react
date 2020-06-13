a09f3b4f40cc6fbaa4dbe194039d5b8d7952a0e6
Porson <porsonlee@gmail.com>
2020-06-13 00:57:24 -0700

    aligned expense tracker and hardcoded to display balance temporarily

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+
+import './App.css'
 
 function App() {
   return (
     <div>
-      Hello World
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </div>
   );
 }
