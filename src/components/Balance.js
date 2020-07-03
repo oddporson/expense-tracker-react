d3b4881e5dd8df217e997c8f673d90a11e6c71eb
Porson <porsonlee@gmail.com>
2020-07-02 23:12:58 -0700

    fixed calculation

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -5,7 +5,9 @@ export const Balance = () => {
   const { transactions } = useContext(GlobalContext);
 
   const amounts = transactions.map(transaction => transaction.amount);
-  const total = amounts.reduce((acc, item) => (acc =+ item), 0).toFixed(2);
+  console.log("amounts", amounts);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  console.log("total", total);
 
   return (
     <div>
